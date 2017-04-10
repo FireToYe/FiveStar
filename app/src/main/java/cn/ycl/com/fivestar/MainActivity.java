78d5b7df5dd5e0b791cf77dbb78b8500db2b1778
yclaide940 <786963531@qq.com>
2017-04-10 16:01:31 +0800

    新增颜色调节器

--- app/src/main/java/cn/ycl/com/fivestar/MainActivity.java
+++ app/src/main/java/cn/ycl/com/fivestar/MainActivity.java
@@ -1,10 +1,14 @@
 package cn.ycl.com.fivestar;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
+
+import static android.animation.ObjectAnimator.ofFloat;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
@@ -19,27 +23,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        final FiveStarView fsv = (FiveStarView) findViewById(R.id.fsv);
-//        tv = (TextView) findViewById(R.id.tv);
-//        fsv.setIntMarkFlag(false);
-//        wcv = (WaveCircleView) findViewById(R.id.wcv);
-//        ofFloat(wcv,"distance",15.f,0.0f).setDuration(8000).start();
-//        wcv.setListener(new WaveCircleView.OnClickListener() {
-//            @Override
-//            public void click() {
-//                ObjectAnimator animator = ObjectAnimator.ofFloat(wcv,"distance",15.f,0.0f).setDuration(8000);
-//                animator.setInterpolator(new DecelerateInterpolator());
-//                animator.start();
-//            }
-//        });
-//        fsv.setListenner(new FiveStarView.OnstarChangeListenner() {
-//            @Override
-//            public void onchangerListener() {
-//                Message message = new Message();
-//                message.what = 0x123;
-//                message.obj = fsv.getStarMark();
-//                handler.sendMessage(message);
-//            }
-//        });
+        final FiveStarView fsv = (FiveStarView) findViewById(R.id.fsv);
+        tv = (TextView) findViewById(R.id.tv);
+        fsv.setIntMarkFlag(false);
+        wcv = (WaveCircleView) findViewById(R.id.wcv);
+        ofFloat(wcv,"distance",15.f,0.0f).setDuration(8000).start();
+        wcv.setListener(new WaveCircleView.OnClickListener() {
+            @Override
+            public void click() {
+                ObjectAnimator animator = ofFloat(wcv,"distance",15.f,0.0f).setDuration(8000);
+                animator.setInterpolator(new DecelerateInterpolator());
+                animator.start();
+            }
+        });
+        fsv.setListenner(new FiveStarView.OnstarChangeListenner() {
+            @Override
+            public void onchangerListener() {
+                Message message = new Message();
+                message.what = 0x123;
+                message.obj = fsv.getStarMark();
+                handler.sendMessage(message);
+            }
+        });
     }
 }
